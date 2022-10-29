20838ef85af6d89ee0df8b21bac4356139fb96dd
Saurabh Sharma <52253058+saurabhsharmaiit123@users.noreply.github.com>
2022-10-29 19:17:44 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (13.38%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-    return set<int>(begin(nums), end(nums)).size() - (count(begin(nums), end(nums), 0) > 0);
-}
+    unordered_set<int>s(nums.begin(),nums.end());
+        return s.size()-s.count(0);
+        
+        
+    }
 };
